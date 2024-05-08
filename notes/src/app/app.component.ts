18efd96a756172f063da26c49c9681240c56d8cb
Emo <emo.2012012@gmail.com>
2024-05-08 13:39:38 +0300

    Notes functions and html completed

---
+++ notes/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+interface Note {
+  title: string;
+  content: string;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Notes';
+
+  notes: Note[] = [];
+  selectedNote: Note | null = null;
+  newNote: Note = { title: '', content: '' };
+  errorMessage: string | null = null;
+  showSaveButton = false;
+
+  selectNote(note: Note) {
+    this.selectedNote = note;
+  }
+
+  editNote(note: Note) {
+    this.selectedNote = note;
+    this.newNote = { ...note };
+    this.showSaveButton = true;
+  }
+
+  saveAddNote() {
+    if (this.validateInput()) {
+      if (this.selectedNote) {
+        const index = this.notes.indexOf(this.selectedNote);
+        this.notes[index] = { ...this.newNote };
+      } else {
+        this.notes.push({ ...this.newNote });
+      }
+      this.clearForm();
+      this.showSaveButton = false;
+    }
+  }
+
+  deleteNote(note: Note) {
+    const index = this.notes.indexOf(note);
+
+    if (index !== -1) {
+      this.notes.splice(index, 1);
+      if (this.selectedNote === note) {
+        this.clearForm();
+      }
+    }
+  }
+
+  validateInput(): boolean {
+    if (this.newNote.title.length < 5 || this.newNote.content.length < 7) {
+      this.errorMessage =
+        'Заглавието трябва да е поне 5 символа, а съдържанието - поне 7 символа.';
+      return false;
+    }
+    this.errorMessage = null;
+    return true;
+  }
+
+  clearForm() {
+    this.newNote = { title: '', content: '' };
+    this.selectedNote = null;
+  }
+}
